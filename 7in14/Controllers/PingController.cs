e5912f6052d880910fbebbf182fc57f64507178d
przemo <curzytekp@gmail.com>
2018-03-26 12:54:01 -0400

    add simple ping endpoint

---
+++ 7in14/Controllers/PingController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace _7in14.Controllers
+{
+    [Route("api/[controller]")]
+    public class PingController
+    {
+        // GET api/values
+        [HttpGet]
+        public string Get()
+        {
+            return "pong";
+        }
+    }
+}
