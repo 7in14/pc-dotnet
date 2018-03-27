d7a8052c7b96c32dd9ce1c3db049eaaf72594d2e
przemo <curzytekp@gmail.com>
2018-03-26 20:27:36 -0400

    configure test coverage task

---
+++ 7in14.Tests/Controllers/PingControllerTests.cs
@@ -0,0 +1,22 @@
+﻿using _7in14.Controllers;
+using FluentAssertions;
+using Xunit;
+
+namespace _7in14.Tests.Controllers
+{
+    public class PingControllerTests
+    {
+        [Fact]
+        public void Ping_ReturnsPong()
+        {
+            // given
+            var controller = new PingController();
+
+            // when
+            var result = controller.Get();
+
+            // then
+            result.Should().Be("pong");
+        }
+    }
+}
